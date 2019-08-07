48d1a0e9e653b82c525452913e8ed9801e3bb1ac
Paul Schaeflein <paul@schaeflein.net>
2019-08-06 19:21:01 -0500

    Update and document sample

---
+++ samples/Utilities/AzureAdOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Graph.Auth;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace Graph.Community.Samples
+{
+	public class AzureAdOptions
+	{
+		public string TenantId { get; set; }
+		public string ClientId { get; set; }
+	}
+}
