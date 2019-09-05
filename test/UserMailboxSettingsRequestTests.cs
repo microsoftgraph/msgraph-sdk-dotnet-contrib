9e54c6956cf4595a4738e85c0481b5297b9fe5e2
Paul Schaeflein <paul@schaeflein.net>
2019-09-05 07:24:24 -0500

    Added UserMailboxSettings

---
+++ test/UserMailboxSettingsRequestTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using Graph.Community;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Graph.Community.Test
+{
+	public class UserMailboxSettingsRequestTests
+	{
+		private readonly ITestOutputHelper output;
+
+		public UserMailboxSettingsRequestTests(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[Fact]
+		public async Task Get_GeneratesCorrectRequest()
+		{
+			// ARRANGE
+			var expectedUri = new Uri("https://graph.microsoft.com/v1.0/me/mailboxSettings");
+
+			using (var response = new HttpResponseMessage())
+			using (var gsc = GraphServiceTestClient.Create(response))
+			{
+				// ACT
+				await gsc.GraphServiceClient
+										.Me
+										.MailboxSettings()
+										.Request()
+										.GetAsync();
+
+				// ASSERT
+				gsc.HttpProvider.Verify(
+					provider => provider.SendAsync(
+						It.Is<HttpRequestMessage>(req =>
+							req.Method == HttpMethod.Get &&
+							req.RequestUri == expectedUri &&
+							req.Headers.Authorization != null
+						),
+						It.IsAny<HttpCompletionOption>(),
+						It.IsAny<CancellationToken>()
+						),
+					Times.Exactly(1)
+				);
+			}
+		}
+	}
+}
