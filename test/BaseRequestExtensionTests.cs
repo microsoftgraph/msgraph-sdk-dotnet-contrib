95bb7a7ae47329a47f55dcd51d126abcea494db9
Paul Schaeflein <paul@schaeflein.net>
2020-02-23 21:28:59 -0600

    fix test name; bump graph version

--- test/BaseRequestExtensionTests.cs
+++ test/BaseRequestExtensionTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -6,49 +6,49 @@ using Xunit;
 
 namespace Graph.Community.Test
 {
-  public class BaseRequestExtensionTests
-  {
-    [Fact]
-    public void GraphRequestGeneratesCorrectRequestHeaders()
-    {
-      // ARRANGE
+	public class BaseRequestExtensionTests
+	{
+		[Fact]
+		public void GraphRequestWithImmutableIdGeneratesCorrectRequestHeaders()
+		{
+			// ARRANGE
 
-      using (var response = new HttpResponseMessage())
-      using (var gsc = GraphServiceTestClient.Create(response))
-      {
-        // ACT
-        var request = gsc.GraphServiceClient
-                            .Me
-                            .Request()
-                            .WithImmutableId()
-                            .GetHttpRequestMessage();
+			using (var response = new HttpResponseMessage())
+			using (var gsc = GraphServiceTestClient.Create(response))
+			{
+				// ACT
+				var request = gsc.GraphServiceClient
+														.Me
+														.Request()
+														.WithImmutableId()
+														.GetHttpRequestMessage();
 
-        // ASSERT
-        Assert.True(request.Headers.Contains(RequestExtensionsConstants.Headers.PreferHeaderName), $"Header does not contain {RequestExtensionsConstants.Headers.PreferHeaderName} header");
-        Assert.Equal(RequestExtensionsConstants.Headers.PreferHeaderImmutableIdValue, string.Join(',', request.Headers.GetValues(RequestExtensionsConstants.Headers.PreferHeaderName)));
-      }
-    }
+				// ASSERT
+				Assert.True(request.Headers.Contains(RequestExtensionsConstants.Headers.PreferHeaderName), $"Header does not contain {RequestExtensionsConstants.Headers.PreferHeaderName} header");
+				Assert.Equal(RequestExtensionsConstants.Headers.PreferHeaderImmutableIdValue, string.Join(',', request.Headers.GetValues(RequestExtensionsConstants.Headers.PreferHeaderName)));
+			}
+		}
 
-    [Fact]
-    public void ExtensionRequestGeneratesCorrectRequestHeaders()
-    {
-      // ARRANGE
+		[Fact]
+		public void ExtensionRequestGeneratesCorrectRequestHeaders()
+		{
+			// ARRANGE
 
-      using (var response = new HttpResponseMessage())
-      using (var gsc = GraphServiceTestClient.Create(response))
-      {
-        // ACT
-        var request = gsc.GraphServiceClient
-                            .SharePointAPI("https://mockSite.sharepoint.com")
-                            .SiteScripts
-                            .Request()
-                            .WithImmutableId()
-                            .GetHttpRequestMessage();
+			using (var response = new HttpResponseMessage())
+			using (var gsc = GraphServiceTestClient.Create(response))
+			{
+				// ACT
+				var request = gsc.GraphServiceClient
+														.SharePointAPI("https://mockSite.sharepoint.com")
+														.SiteScripts
+														.Request()
+														.WithImmutableId()
+														.GetHttpRequestMessage();
 
-        // ASSERT
-        Assert.True(request.Headers.Contains(RequestExtensionsConstants.Headers.PreferHeaderName), $"Header does not contain {RequestExtensionsConstants.Headers.PreferHeaderName} header");
-        Assert.Equal(RequestExtensionsConstants.Headers.PreferHeaderImmutableIdValue, string.Join(',', request.Headers.GetValues(RequestExtensionsConstants.Headers.PreferHeaderName)));
-      }
-    }
-  }
+				// ASSERT
+				Assert.True(request.Headers.Contains(RequestExtensionsConstants.Headers.PreferHeaderName), $"Header does not contain {RequestExtensionsConstants.Headers.PreferHeaderName} header");
+				Assert.Equal(RequestExtensionsConstants.Headers.PreferHeaderImmutableIdValue, string.Join(',', request.Headers.GetValues(RequestExtensionsConstants.Headers.PreferHeaderName)));
+			}
+		}
+	}
 }
