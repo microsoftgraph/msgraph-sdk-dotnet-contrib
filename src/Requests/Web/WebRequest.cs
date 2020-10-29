4b9477b587bf2885fa80cf8b031262e90c47c7cd
Paul Schaeflein <paul@schaeflein.net>
2020-10-28 23:42:29 -0500

    Add Expand() to WebRequest

--- src/Requests/Web/WebRequest.cs
+++ src/Requests/Web/WebRequest.cs
@@ -1,6 +1,7 @@
 using Microsoft.Graph;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -8,74 +9,110 @@ using System.Threading.Tasks;
 
 namespace Graph.Community
 {
-  public class WebRequest : BaseRequest, IWebRequest
-  {
+	public class WebRequest : BaseRequest, IWebRequest
+	{
 #pragma warning disable CA1054 // URI parameters should not be strings
-    public WebRequest(
-      string requestUrl,
-      IBaseClient client,
-      IEnumerable<Option> options)
-      : base(requestUrl, client, options)
-    {
-      this.Headers.Add(new HeaderOption(SharePointAPIRequestConstants.Headers.AcceptHeaderName, SharePointAPIRequestConstants.Headers.AcceptHeaderValue));
-      this.Headers.Add(new HeaderOption(SharePointAPIRequestConstants.Headers.ODataVersionHeaderName, SharePointAPIRequestConstants.Headers.ODataVersionHeaderValue));
-    }
+		public WebRequest(
+			string requestUrl,
+			IBaseClient client,
+			IEnumerable<Option> options)
+			: base(requestUrl, client, options)
+		{
+			this.Headers.Add(new HeaderOption(SharePointAPIRequestConstants.Headers.AcceptHeaderName, SharePointAPIRequestConstants.Headers.AcceptHeaderValue));
+			this.Headers.Add(new HeaderOption(SharePointAPIRequestConstants.Headers.ODataVersionHeaderName, SharePointAPIRequestConstants.Headers.ODataVersionHeaderValue));
+		}
 #pragma warning restore CA1054 // URI parameters should not be strings
 
-    public Task<Web> GetAsync()
-    {
-      return this.GetAsync(CancellationToken.None);
-    }
+		public Task<Web> GetAsync()
+		{
+			return this.GetAsync(CancellationToken.None);
+		}
 
-    public async Task<Web> GetAsync(CancellationToken cancellationToken)
-    {
-      this.ContentType = "application/json";
-      var entity = await this.SendAsync<Graph.Community.Web>(null, cancellationToken).ConfigureAwait(false);
-      return entity;
-    }
+		public async Task<Web> GetAsync(CancellationToken cancellationToken)
+		{
+			this.ContentType = "application/json";
+			var entity = await this.SendAsync<Graph.Community.Web>(null, cancellationToken).ConfigureAwait(false);
+			return entity;
+		}
 
-    public Task<ICollectionPage<Change>> GetChangesAsync(ChangeQuery query)
-    {
-      return this.GetChangesAsync(query, CancellationToken.None);
-    }
-    public async Task<ICollectionPage<Change>> GetChangesAsync(ChangeQuery query, CancellationToken cancellationToken)
-    {
-      return await ChangeLogRequest.GetChangesAsync(this, query, cancellationToken).ConfigureAwait(false);
-    }
+		public Task<ICollectionPage<Change>> GetChangesAsync(ChangeQuery query)
+		{
+			return this.GetChangesAsync(query, CancellationToken.None);
+		}
+		public async Task<ICollectionPage<Change>> GetChangesAsync(ChangeQuery query, CancellationToken cancellationToken)
+		{
+			return await ChangeLogRequest.GetChangesAsync(this, query, cancellationToken).ConfigureAwait(false);
+		}
 
-    public async Task<User> EnsureUserAsync(string logonName)
-    {
-      return await this.EnsureUserAsync(logonName, CancellationToken.None);
-    }
+		public async Task<User> EnsureUserAsync(string logonName)
+		{
+			return await this.EnsureUserAsync(logonName, CancellationToken.None);
+		}
 
-    public async Task<User> EnsureUserAsync(string logonName, CancellationToken cancellationToken)
+		public async Task<User> EnsureUserAsync(string logonName, CancellationToken cancellationToken)
+		{
+			if (string.IsNullOrEmpty(logonName))
+			{
+				throw new ArgumentNullException(nameof(logonName));
+			}
+
+			this.AppendSegmentToRequestUrl("ensureuser");
+			this.Method = HttpMethod.Post.Method;
+			this.ContentType = "application/json";
+
+			var payload = new { logonName = logonName };
+			var userEntity = await this.SendAsync<User>(payload, cancellationToken);
+			return userEntity;
+		}
+
+		public async Task<Web> GetAssociatedGroupsAsync()
     {
-      if (string.IsNullOrEmpty(logonName))
+			return await GetAssociatedGroupsAsync(false, CancellationToken.None);
+		}
+
+		public async Task<Web> GetAssociatedGroupsAsync(bool includeUsers = false)
+		{
+			return await GetAssociatedGroupsAsync(includeUsers, CancellationToken.None);
+		}
+
+		public async Task<Web> GetAssociatedGroupsAsync(bool includeUsers, CancellationToken cancellationToken)
+		{
+			string expand = "associatedownergroup,associatedmembergroup,associatedvisitorgroup";
+      if (includeUsers)
       {
-        throw new ArgumentNullException(nameof(logonName));
-      }
+				expand += ",associatedownergroup/users,associatedmembergroup/users,associatedvisitorgroup/users";
+			}
+			this.QueryOptions.Add(new QueryOption("$expand", expand));
+			this.QueryOptions.Add(new Microsoft.Graph.QueryOption("$select", "id,title,associatedownergroup,associatedmembergroup,associatedvisitorgroup"));
 
-      this.AppendSegmentToRequestUrl("ensureuser");
-      this.Method = HttpMethod.Post.Method;
-      this.ContentType = "application/json";
+			var web = await this.SendAsync<Web>(null, cancellationToken);
+			return web;
+		}
 
-      var payload = new { logonName = logonName };
-      var userEntity = await this.SendAsync<User>(payload, cancellationToken);
-      return userEntity;
-    }
 
-    public async Task<Web> GetAssociatedGroupsAsync()
-    {
-      return await GetAssociatedGroupsAsync(CancellationToken.None);
-    }
+		public IWebRequest Expand(string value)
+		{
+			this.QueryOptions.Add(new QueryOption("$expand", value));
+			return this;
+		}
 
-    public async Task<Web> GetAssociatedGroupsAsync(CancellationToken cancellationToken)
-    {
-      this.QueryOptions.Add(new QueryOption("$expand", "associatedownergroup,associatedmembergroup,associatedvisitorgroup"));
-      this.QueryOptions.Add(new Microsoft.Graph.QueryOption("$select", "associatedownergroup,associatedmembergroup,associatedvisitorgroup"));
-
-      var web = await this.SendAsync<Web>(null, cancellationToken);
-      return web;
-    }
-  }
+		public IWebRequest Expand(Expression<Func<Group, object>> expandExpression)
+		{
+			if (expandExpression == null)
+			{
+				throw new ArgumentNullException(nameof(expandExpression));
+			}
+			string error;
+			string value = ExpressionExtractHelper.ExtractMembers(expandExpression, out error);
+			if (value == null)
+			{
+				throw new ArgumentException(error, nameof(expandExpression));
+			}
+			else
+			{
+				this.QueryOptions.Add(new QueryOption("$expand", value));
+			}
+			return this;
+		}
+	}
 }
