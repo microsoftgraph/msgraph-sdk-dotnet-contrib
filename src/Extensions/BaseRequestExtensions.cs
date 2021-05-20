71168a4bab477929bb2769f2aec9829c66713d9f
Paul Schaeflein <paul@schaeflein.net>
2021-05-19 22:56:31 -0500

    Add ConsistencyLevel header extension

--- src/Extensions/BaseRequestExtensions.cs
+++ src/Extensions/BaseRequestExtensions.cs
@@ -7,6 +7,12 @@ namespace Graph.Community
 {
 	public static class BaseRequestExtensions
 	{
+		/// <summary>
+		/// Adds the <b>Prefer</b> header to include Immutable Ids in the response
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="baseRequest">The <see cref="BaseRequest"/> for the request.</param>
+		/// <returns></returns>
 		public static T WithImmutableId<T>(this T baseRequest) where T : IBaseRequest
 		{
 			baseRequest.Headers.Add(
@@ -16,5 +22,22 @@ namespace Graph.Community
 			);
 			return baseRequest;
 		}
+
+		/// <summary>
+		/// Applies the <b>ConsistencyLevel</b> header required for certain DirectoryObject queries.
+		/// (https://developer.microsoft.com/en-us/office/blogs/microsoft-graph-advanced-queries-for-directory-objects-are-now-generally-available/)
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="baseRequest">The <see cref="BaseRequest"/> for the request.</param>
+		/// <returns></returns>
+		public static T WithEventualConsistency<T>(this T baseRequest) where T: IBaseRequest
+    {
+			baseRequest.Headers.Add(
+				new HeaderOption(
+					"ConsistencyLevel",
+					"eventual")
+			);
+			return baseRequest;
+    }
 	}
 }
