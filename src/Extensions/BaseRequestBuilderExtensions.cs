6870c36b93bdd994ef86948190d05cd35068b10f
Paul Schaeflein <paul@schaeflein.net>
2021-05-19 22:49:06 -0500

    Add OData cast to request builder

--- src/Extensions/BaseRequestBuilderExtensions.cs
+++ src/Extensions/BaseRequestBuilderExtensions.cs
@@ -16,5 +16,20 @@ namespace Graph.Community
 			var pathAndQuery = new Uri(requestBuilder.RequestUrl).PathAndQuery;
 			return pathAndQuery.Substring(pathAndQuery.IndexOf('/', 1)); //skips first / to ignore the version
 		}
+
+		/// <summary>
+		/// Applies an OData cast filter to the returned collection.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="requestBuilder">Current request builder</param>
+		/// <param name="oDataCast">The OData type name</param>
+		/// <returns>Request builder with OData cast filter applied</returns>
+		public static T WithODataCast<T>(this T requestBuilder, string oDataCast) where T : IBaseRequestBuilder
+    {
+			var updatedUrl = requestBuilder.AppendSegmentToRequestUrl(oDataCast);
+			var updatedBuilder = (T)Activator.CreateInstance(requestBuilder.GetType(), updatedUrl, requestBuilder.Client);
+
+			return updatedBuilder;
+		}
 	}
 }
