aedf9f7abe9b926f763da86a628ab915085ea2e5
Paul Schaeflein <paul@schaeflein.net>
2021-07-30 10:40:16 -0500

    Update SiteDesignMetadata model; Bump GraphSDK version

--- src/Models/SiteDesignMetadata.cs
+++ src/Models/SiteDesignMetadata.cs
@@ -9,49 +9,66 @@ namespace Graph.Community
 #pragma warning disable CA1056 // Uri properties should not be strings
 #pragma warning disable CA2227 // Collection properties should be read only
 
-  [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
   public class SiteDesignMetadata : BaseItem
   {
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "DesignPackageId", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "DesignPackageId", NullValueHandling = NullValueHandling.Ignore)]
     public Guid DesignPackageId { get; set; }
+
+    public string DesignType { get; set; }
 
     /// <summary>
     /// (Optional) True if the site design is applied as the default site design; otherwise, false. For more information see Customize a default site design.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "IsDefault", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "IsDefault", NullValueHandling = NullValueHandling.Ignore)]
     public bool IsDefault { get; set; }
+
+    public bool IsOutOfBoxTemplate { get; set; }
+    public bool IsTenantAdminOnly { get; set; }
+    public string ListColor { get; set; }
+    public string ListIcon { get; set; }
+
 
     /// <summary>
     /// (Optional) The alt text description of the image for accessibility.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "PreviewImageAltText", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "PreviewImageAltText", NullValueHandling = NullValueHandling.Ignore)]
     public string PreviewImageAltText { get; set; }
 
     /// <summary>
     /// (Optional) The URL of a preview image. If none is specified, SharePoint uses a generic image.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "PreviewImageUrl", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "PreviewImageUrl", NullValueHandling = NullValueHandling.Ignore)]
     public string PreviewImageUrl { get; set; }
+
+    public bool RequiresGroupConnected { get; set; }
+    public bool RequiresTeamsConnected { get; set; }
+    public bool RequiresYammerConnected { get; set; }
 
     /// <summary>
     /// An array of one or more site scripts. Each is identified by an ID. The scripts will run in the order listed.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "SiteScriptIds", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "SiteScriptIds", NullValueHandling = NullValueHandling.Ignore)]
     public List<Guid> SiteScriptIds { get; set; }
+
+    public string[] SupportedWebTemplates { get; set; }
+    public string[] TemplateFeatures { get; set; }
+    public string ThumbnailUrl { get; set; }
 
     /// <summary>
     /// The display name of the site design.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Title", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "Title", NullValueHandling = NullValueHandling.Ignore)]
     public string Title { get; set; }
 
     /// <summary>
     /// Identifies which base template to add the design to. Use the value 64 for the Team site template, and the value 68 for the Communication site template.
     /// </summary>
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "WebTemplate", Required = Newtonsoft.Json.Required.Default)]
+    [JsonProperty(PropertyName = "WebTemplate", NullValueHandling = NullValueHandling.Ignore)]
     public string WebTemplate { get; set; }
 
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Version", Required = Newtonsoft.Json.Required.Default)]
+    public string Order { get; set; }
+
+    [JsonProperty(PropertyName = "Version", NullValueHandling = NullValueHandling.Ignore)]
     public int Version { get; set; }
   }
 
