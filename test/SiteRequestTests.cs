c02dfeed814378bf49d9bbc80e8a1995a8c87ead
Paul Schaeflein <paul@schaeflein.net>
2023-10-23 07:46:19 -0500

    Samples update (#61)
    * Additional tests for Site
    
    * Bump samples dependencies
    
    * Merge upstream
    
    * Update samples
    
    ---------
    
    Co-authored-by: Michael Mainer <8527305+MIchaelMainer@users.noreply.github.com>

--- test/SiteRequestTests.cs
+++ test/SiteRequestTests.cs
@@ -1,5 +1,6 @@
 using Moq;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -37,6 +38,36 @@ namespace Graph.Community.Test
       Assert.Equal(SharePointAPIRequestConstants.Headers.AcceptHeaderValue, request.Headers.Accept.ToString());
       Assert.True(request.Headers.Contains(SharePointAPIRequestConstants.Headers.ODataVersionHeaderName), $"Header does not contain {SharePointAPIRequestConstants.Headers.ODataVersionHeaderName} header");
       Assert.Equal(SharePointAPIRequestConstants.Headers.ODataVersionHeaderValue, string.Join(',', request.Headers.GetValues(SharePointAPIRequestConstants.Headers.ODataVersionHeaderName)));
+    }
+
+    [Fact]
+    public async void Get_ReturnsCorrectResponse()
+    {
+      // ARRANGE
+      var responseContent = ResourceManager.GetHttpResponseContent("GetSiteResponse.json");
+      var responseMessage = new HttpResponseMessage()
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(responseContent),
+      };
+
+      using (responseMessage)
+      using (TestGraphServiceClient gsc = TestGraphServiceClient.Create(responseMessage))
+      {
+        // ACT
+        var actual = await gsc.GraphServiceClient
+                                  .SharePointAPI(mockWebUrl)
+                                  .Site
+                                  .Request()
+                                  .GetAsync();
+
+        // ASSERT
+        Assert.Equal("Internal Only", actual.Classification);
+        Assert.Equal("00000000-0000-0000-0000-000000000000", actual.GroupId);
+        Assert.Equal("354401ad-7245-45c6-afae-8ff465b66807", actual.HubSiteId);
+        Assert.False(actual.IsHubSite);
+        Assert.Equal("/sites/mockSite", actual.ServerRelativeUrl);
+      }
     }
 
     [Fact]
